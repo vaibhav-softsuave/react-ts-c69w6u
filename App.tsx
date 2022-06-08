843111976bc3eab2e8bccf97bb1d4e3d26350318
vaibhav-softsuave <vaibhav.kumarsingh@softsuave.com>
2022-06-08 19:09:41 +0530

    add onclick counter

--- App.tsx
+++ App.tsx
@@ -2,10 +2,15 @@ import * as React from 'react';
 import './style.css';
 
 export default function App() {
+  const [i,setI] = React.useState(0)
+  function count(i){
+     setI(i+1)
+  }
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
+      <button onClick={()=>count(i)}  >click me! {i}</button>
     </div>
   );
 }
